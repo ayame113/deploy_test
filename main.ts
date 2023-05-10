6b16492d464af8408d5e3fd0e18d15aa41b2dba2
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-05-10 00:38:30 +0000

    test

--- main.ts
+++ main.ts
@@ -6,10 +6,11 @@ import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
   console.log("Deno.readFile", performance.now() - start);
 }
 
+const url = new URL(import.meta.resolve("./files"));
 {
   const start = performance.now();
-  for await (const entry of Deno.readDir(new URL(import.meta.resolve("./")))) {
-    console.log(await Deno.stat(entry.name));
+  for await (const entry of Deno.readDir(url)) {
+    await Deno.stat("./files/" + entry.name);
   }
   console.log("serial Deno.readDir", performance.now() - start);
 }
@@ -17,11 +18,11 @@ import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
 {
   const start = performance.now();
   const statPromise = [];
-  for await (const entry of Deno.readDir(new URL(import.meta.resolve("./")))) {
-    statPromise.push(Deno.stat(entry.name));
+  for await (const entry of Deno.readDir(url)) {
+    statPromise.push(Deno.stat("./files/" + entry.name));
   }
   await Promise.all(statPromise);
-  console.log("serial Deno.readDir", performance.now() - start);
+  console.log("paralell Deno.readDir", performance.now() - start);
 }
 
 serve((_req) => Response.json("hello"));
