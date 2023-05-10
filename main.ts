0819724f5437bed9f1c4c718cbce772747f2d23c
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-05-10 00:25:51 +0000

    serve

---
+++ main.ts
@@ -0,0 +1,25 @@
+{
+  const start = performance.now();
+  await Deno.readFile(new URL(import.meta.resolve("./README.md")));
+  console.log("Deno.readFile", performance.now() - start);
+}
+
+{
+  const start = performance.now();
+  for await (const entry of Deno.readDir(new URL(import.meta.resolve("./")))) {
+    console.log(await Deno.stat(entry.name));
+  }
+  console.log("serial Deno.readDir", performance.now() - start);
+}
+
+{
+  const start = performance.now();
+  const statPromise = [];
+  for await (const entry of Deno.readDir(new URL(import.meta.resolve("./")))) {
+    statPromise.push(Deno.stat(entry.name));
+  }
+  await Promise.all(statPromise);
+  console.log("serial Deno.readDir", performance.now() - start);
+}
+
+Deno.serve((_req) => Response.json("hello"));
